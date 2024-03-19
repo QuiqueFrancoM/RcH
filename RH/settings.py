7bf5a0a14e178b88608be8f357f2380f7aad4444
QuiqueFrancoM <142038815+QuiqueFrancoM@users.noreply.github.com>
2024-03-18 18:16:52 -0600

    sunday

--- RH/settings.py
+++ RH/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+
+STATIC_FILES_DIRS = (BASE_DIR / 'static')
 
 
 # Default primary key field type
